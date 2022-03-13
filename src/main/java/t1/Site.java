840dbd778ac735f3770d12ba264f00d42639f697
毛 <1296193245@qq.com>
2022-03-13 21:44:33 +0800

    MyBatis_choose_when_and_otherwise

--- src/main/java/t1/Site.java
+++ src/main/java/t1/Site.java
@@ -14,7 +14,6 @@ import java.util.Date;
  * Version(版本): 1.0
  * Description(描述)： 无
  */
-
 public class Site
 {
     private Integer id;
@@ -51,61 +50,121 @@ public class Site
         this.createTime = createTime;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public Integer getId()
     {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(Integer id)
     {
         this.id = id;
     }
 
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
     public String getName()
     {
         return name;
     }
 
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
     public void setName(String name)
     {
         this.name = name;
     }
 
+    /**
+     * Gets url.
+     *
+     * @return the url
+     */
     public String getUrl()
     {
         return url;
     }
 
+    /**
+     * Sets url.
+     *
+     * @param url the url
+     */
     public void setUrl(String url)
     {
         this.url = url;
     }
 
+    /**
+     * Gets age.
+     *
+     * @return the age
+     */
     public Integer getAge()
     {
         return age;
     }
 
+    /**
+     * Sets age.
+     *
+     * @param age the age
+     */
     public void setAge(Integer age)
     {
         this.age = age;
     }
 
+    /**
+     * Gets country.
+     *
+     * @return the country
+     */
     public String getCountry()
     {
         return country;
     }
 
+    /**
+     * Sets country.
+     *
+     * @param country the country
+     */
     public void setCountry(String country)
     {
         this.country = country;
     }
 
+    /**
+     * Gets create time.
+     *
+     * @return the create time
+     */
     public Date getCreateTime()
     {
         return createTime;
     }
 
+    /**
+     * Sets create time.
+     *
+     * @param createTime the create time
+     */
     public void setCreateTime(Date createTime)
     {
         this.createTime = createTime;
--- src/main/java/t1/SiteMapper.java
+++ src/main/java/t1/SiteMapper.java
@@ -18,20 +18,72 @@ import java.util.List;
  * Description(描述)： 无
  */
 
+
 public interface SiteMapper
 {
+    /**
+     * Update int.
+     *
+     * @param site the site
+     * @return the int
+     */
     public int update(@Param("site") Site site);
 
+    /**
+     * Update by param int.
+     *
+     * @param id         the id
+     * @param name       the name
+     * @param url        the url
+     * @param age        the age
+     * @param country    the country
+     * @param createTime the create time
+     * @return the int
+     */
     public int updateByParam(@Param("id") Integer id, @Param("name") String name, @Param("url") String url,
                              @Param("age") Integer age, @Param("country") String country, @Param("createTime") Date createTime);
 
+    /**
+     * Select list.
+     *
+     * @param site the site
+     * @return the list
+     */
     public List<Site> select(@Param("site") Site site);
 
+    /**
+     * Select by param list.
+     *
+     * @param id         the id
+     * @param name       the name
+     * @param url        the url
+     * @param age        the age
+     * @param country    the country
+     * @param createTime the create time
+     * @return the list
+     */
     public List<Site> selectByParam(@Param("id") Integer id, @Param("name") String name, @Param("url") String url,
                                     @Param("age") Integer age, @Param("country") String country, @Param("createTime") Date createTime);
 
+    /**
+     * Select website list.
+     *
+     * @param site the site
+     * @return the list
+     */
     public List<Site> selectWebsite(Site site);
 
+    /**
+     * Select website by param list.
+     *
+     * @param id         the id
+     * @param name       the name
+     * @param url        the url
+     * @param age        the age
+     * @param country    the country
+     * @param createTime the create time
+     * @return the list
+     */
     public List<Site> selectWebsiteByParam(@Param("id") Integer id, @Param("name") String name, @Param("url") String url,
                                            @Param("age") Integer age, @Param("country") String country, @Param("createTime") Date createTime);
 }
